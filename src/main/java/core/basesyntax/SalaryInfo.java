2b8f9df18bbb6c28820cea65c73747a5bfcd0e24
Maksym <pozetiv13@gmail.com>
2026-03-22 15:13:25 +0200

    Maksym Gorodnychyi

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,42 +1,43 @@
 package core.basesyntax;
 
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+
 public class SalaryInfo {
+    private static final int DATE_INDEX = 0;
+    private static final int NAME_INDEX = 1;
+    private static final int HOURS_INDEX = 2;
+    private static final int RATE_INDEX = 3;
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        StringBuilder result = new StringBuilder("Report for period " + dateFrom + " - " + dateTo + "\n");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate fromDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate toDate = LocalDate.parse(dateTo, formatter);
+        StringBuilder result = new StringBuilder(
+                "Report for period " + dateFrom + " - " + dateTo + LINE_SEPARATOR);
 
         for (String name : names) {
             int totalSalary = 0;
 
             for (String record : data) {
                 String[] parts = record.split(" ");
-                String recordDate = parts[0];
-                String recordName = parts[1];
-                String recordHours = parts[2];
-                String recordRate = parts[3];
-                int hours = Integer.parseInt(recordHours);
-                int rate = Integer.parseInt(recordRate);
-                String day = recordDate.substring(0, 2);
-                String month = recordDate.substring(3, 5);
-                String year = recordDate.substring(6, 10);
-                String formatedRecordDate = year + month + day;
-                String fromDay = dateFrom.substring(0, 2);
-                String froMonth = dateFrom.substring(3, 5);
-                String fromYear = dateFrom.substring(6, 10);
-                String formatedDateFrom = fromYear + froMonth + fromDay;
-                String toDateDay = dateTo.substring(0, 2);
-                String toDateMonth = dateTo.substring(3, 5);
-                String toDateYear = dateTo.substring(6, 10);
-                String formatedDayTo = toDateYear + toDateMonth + toDateDay;
-                int formatedRecordData = Integer.parseInt(formatedRecordDate);
-                int formatedDataFrom = Integer.parseInt(formatedDateFrom);
-                int formatedDaTo = Integer.parseInt(formatedDayTo);
+                String recordDate = parts[DATE_INDEX];
+                String recordName = parts[NAME_INDEX];
+                LocalDate currentDate = LocalDate.parse(recordDate, formatter);
+                boolean isAfterOrEqualDate = currentDate.isAfter(fromDate) || currentDate.isEqual(fromDate);
+                boolean isBeforeOrEqualDate = currentDate.isBefore(toDate) || currentDate.isEqual(toDate);
+                int hours = Integer.parseInt(parts[HOURS_INDEX]);
+                int rate = Integer.parseInt(parts[RATE_INDEX]);
 
-                if (recordName.equals(name) && formatedRecordData >= formatedDataFrom && formatedRecordData <= formatedDaTo) {
+                if (recordName.equals(name)
+                        && isAfterOrEqualDate
+                        && isBeforeOrEqualDate) {
                     totalSalary += hours * rate;
                 }
 
             }
-            result.append(name).append(" - ").append(totalSalary).append("\n");
+            result.append(name).append(" - ").append(totalSalary).append(LINE_SEPARATOR);
         }
         return result.toString();
     }
